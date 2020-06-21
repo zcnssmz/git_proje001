10ee8d8559d2dde55f2230cf8657fa6947f7162b
zcnssmz <ozcansasmaz@gmail.com>
2020-06-21 15:33:21 -0400

    Featur branch'tan ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from repository");
 		System.out.println("ikinci commit from repository");
+		System.out.println("yeni branch oluşturdum ve adı feature");
 
 	}
 
