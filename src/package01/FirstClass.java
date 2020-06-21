484f2f4139529e408a14f5084adc3ea165345ffa
zcnssmz <ozcansasmaz@gmail.com>
2020-06-21 15:16:00 -0400

    ikinci commit local repositorydennn

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit from repository");
+		System.out.println("ikinci commit from repository");
 
 	}
 
